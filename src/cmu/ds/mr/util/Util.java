26f380c4bb4d12bacc9ad9c8f44985d933e78028
Zeyuan Li <zyli5313@gmail.com>
2013-04-11 20:23:04 -0400

    fix rm -r

--- src/cmu/ds/mr/util/Util.java
+++ src/cmu/ds/mr/util/Util.java
@@ -69,12 +69,8 @@ public class Util {
 
       // directory is empty, then delete it
       if (file.list().length == 0) {
-
         file.delete();
-        System.out.println("Directory is deleted : " + file.getAbsolutePath());
-
       } else {
-
         // list all the directory contents
         String files[] = file.list();
 
@@ -89,14 +85,12 @@ public class Util {
         // check the directory again, if empty then delete it
         if (file.list().length == 0) {
           file.delete();
-          System.out.println("Directory is deleted : " + file.getAbsolutePath());
         }
       }
 
     } else {
       // if file, then delete it
       file.delete();
-      System.out.println("File is deleted : " + file.getAbsolutePath());
     }
   }
 
